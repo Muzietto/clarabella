88a2075df1f2c251bb1883ba98be9e482301b451
Muzietto <marco_faustinelli@yahoo.com>
2017-03-14 10:59:19 +0100

    IT WORKS!!!

--- sandbox/mytodos/src/index.js
+++ sandbox/mytodos/src/index.js
@@ -3,4 +3,17 @@ import { createStore } from 'redux'
 import { todoApp, initialState } from './reducers'
 import { VisibilityFilters, ActionTypes, ActionCreators } from './actions'
 
-let store = createStore(todoApp/*, initialStateFromServer*/)
+let store = createStore(todoApp, initialState)
+
+console.log(store.getState())
+
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState()));
+
+store.dispatch(ActionCreators.addTodo('ciao mamma'))
+
+store.dispatch(ActionCreators.setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+store.dispatch(ActionCreators.toggleTodo(0))
+
+unsubscribe()
